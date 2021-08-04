58611eabc1122a0daab868c182687b798666320d
Raaed Kabir <raaed.kabir@gmail.com>
2021-08-05 02:04:12 +0800

    added Notifications page

--- src/views/Dashboard/Notifications.vue
+++ src/views/Dashboard/Notifications.vue
@@ -1,6 +1,9 @@
 <template>
-  <div>
-    <h1>Notifications</h1>
+  <div class="container">
+    <div v-if="imageMissing" class="alert alert__error spacer">
+      <mdicon name="alert-circle" />
+      <p class="alert__text">Missing Photo</p>
+    </div>
   </div>
 </template>
 
@@ -9,9 +12,44 @@ import { defineComponent } from 'vue';
 
 export default defineComponent({
   setup() {
-    return {};
+    const imageMissing = !localStorage.getItem('imageLink');
+
+    return {
+      imageMissing,
+    };
   },
 });
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.container {
+  margin: 0 2rem;
+}
+
+//variables
+$alertPrimary: #ccf2ff;
+$alertSecondary: #e2e3e3;
+$alertWarning: #ffff99;
+$alertError: #ffb3b3;
+$alertSuccess: #d6f5d6;
+
+.alert {
+  width: 100%;
+  border-radius: 3px;
+  padding: 1rem 2rem;
+  color: black;
+
+  .mdi {
+    margin-right: 1rem;
+  }
+
+  &__text {
+    display: inline-block;
+    font-size: 1.4rem;
+  }
+
+  &__error {
+    background-color: $alertError;
+  }
+}
+</style>
