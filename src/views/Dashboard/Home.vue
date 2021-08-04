099b96a747e20bbf59048a1122048381c805e52e
Raaed Kabir <raaed.kabir@gmail.com>
2021-08-05 02:04:54 +0800

    added Home page

--- src/views/Dashboard/Home.vue
+++ src/views/Dashboard/Home.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <h1>Home</h1>
+  <div class="container">
+    <h1>Welcome {{ first_name }} {{ last_name }}!</h1>
   </div>
 </template>
 
@@ -9,9 +9,19 @@ import { defineComponent } from 'vue';
 
 export default defineComponent({
   setup() {
-    return {};
+    const first_name = localStorage.getItem('fName');
+    const last_name = localStorage.getItem('lName');
+
+    return {
+      first_name,
+      last_name,
+    };
   },
 });
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.container {
+  margin-left: 2rem;
+}
+</style>
